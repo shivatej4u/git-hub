ac57797494040bee54c4430e4679ad2833e87422
shivatej4u <shivatej4u@gmail.com>
2015-10-19 21:33:10 -0400

    sql
    hghjgjhg

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+select * from departments
+insert into departments values('shiva','india');
+insert into departments values('shiv','indi');
+insert into departments values('shi','ind');
+select * from employees
+
+insert into employees values ('ravi','kalali',2332,1);
+insert into employees values ('rav','kalal',233,2);
+insert into employees values ('ra','lali',32,3);
+insert into employees values ('r','kal',2,2);
